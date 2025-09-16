5135b35364e761a833c14e983d35649662b16ac9
Saurabh Singh <123312228+Saurabhcode21@users.noreply.github.com>
2025-09-16 22:40:47 +0530

    2-commit

---
+++ pettern.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std ;
+
+// int main(){
+//     int n;
+//     cout<<"enter the value of N";
+//     cin>>n;
+
+//      int i=1;
+//      while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<j;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//      }
+// }
+
+// int main(){
+//     int n;
+//     cout<<"enter the value of N";
+//     cin>>n;
+//     int i =1;
+//     while(i<=n){
+//          int j=1;
+//          while(j<=n){
+//             cout<<n-j+1;
+//             j++;
+//          }
+//          cout<<endl;
+//          i++;
+//     }
+// }
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter the value of N";
+//     cin>>n;
+//     int sum =1;
+//     int i= 1;
+//     while(i<=n){
+//         int j =1;
+//         while(j<=n){
+//             cout<<sum;
+//             sum++;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i=1;
+//     while(i<=4){
+//         int j=1;
+//         while (j<=i){
+//             cout<<"x";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
