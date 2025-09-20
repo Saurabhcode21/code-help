8148e92fdbfdb46c0a28ff2c16f2c4c00ae4e76c
Saurabh Singh <123312228+Saurabhcode21@users.noreply.github.com>
2025-09-20 14:39:41 +0530

    Update pettern.cpp

--- pettern.cpp
+++ pettern.cpp
@@ -66,3 +66,144 @@ using namespace std ;
 //     }
 // }
 
+// int main(){
+//     int i=1;
+//     while(i<=4){
+//         int j =1;
+//         while(j<=i){ 
+//             cout<<i;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//    int i=1;
+//    while(i<=4){
+//     int j=1; 
+//     int value = i;
+//     while(j<=i){
+//         cout<<value;
+//         value++;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+//    }
+// }
+
+// int main(){
+//     int i =1;
+//     while(i<=4){
+//         int j=1;
+//         while(j<=i){
+//             cout<<i-j+1;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i=1;
+//     while(i<=4){
+//         int j=1; 
+//         while(j<=4){
+
+//             char value = 'A'+i-1;
+//             cout<<value;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i=1;
+//     while(i<=4){
+//         int j=1;
+//         while(j<=4){
+//             char ch = 'A'+j-1;
+//             cout<<ch;
+            
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i=1;
+//     char start = 'A';
+//     while(i<=4){
+//         int j =1; 
+//         while(j<=4){
+//             cout<<start;
+//             start++;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i = 1;
+//     while(i<=4){
+//         int j=1; 
+//         while(j<=4){
+//             char ch = 'A' +i+j-2;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i =1;
+//     while(i<=4){
+//         int j =1;
+//         while(j<=i){
+//             char ch = 'A' +i-1;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int i=1;
+//     while(i<=4){
+//         int j= 1;
+//         while(j<=i){
+//             char ch = 'A'+i+j-2;
+//             cout<<ch;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+int main(){
+    int i=1;
+    while(i<=4){
+        int j=1;   char ch = 'A' +4-i;
+        while(j<=i){
+            cout<<ch;
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
