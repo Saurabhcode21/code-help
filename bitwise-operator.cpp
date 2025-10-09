6c3c30680e4aca076819717ae4792607cd04928c
saurabh kumar <saurabhkm12345@gmail.com>
2025-10-09 15:54:59 +0530

    update file

---
+++ bitwise-operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4;
+    int b = 6;
+
+    cout<<"a&b = "<<(a&b)<<endl;
+    cout<<"a|b = "<<(a|b)<<endl;
+    cout<<"~a = "<<(~a)<<endl;
+    cout<<"a^b = "<<(a^b)<<endl;
+
+
+    cout<<(3<<2)<<endl;
+    cout<<(5<<2)<<endl;
+    cout<<(5>>2)<<endl;
+    cout<<(4>>3)<<endl;
+
+}
